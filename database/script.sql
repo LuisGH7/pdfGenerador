3541e4fa22dc460bebbfb35572f937484e007486
Luis Anderson Boada Ramos <luisbo_56@hotmail.com>
2022-11-10 18:39:07 -0500

    json en BD

--- database/script.sql
+++ database/script.sql
@@ -54,3 +54,50 @@ ORDER BY idcomentario DESC LIMIT 1;
 END$$
 
 CALL spu_comentario_listar();
+
+CREATE TABLE equipos
+(
+	idequipo					INT 				AUTO_INCREMENT PRIMARY KEY,
+	nombre 					VARCHAR(30)		NOT NULL,
+	precio 					DECIMAL(7,2)	NOT NULL,
+	garantia 				TINYINT			NOT NULL,
+	especificaciones 		JSON	 			NULL
+)ENGINE=INNODB;
+
+INSERT INTO equipos (nombre, precio, garantia, especificaciones) VALUES
+("Samsung Galaxy S3",1200,12,'{"capacidad":512, "ram": 1024}')
+
+INSERT INTO equipos (nombre, precio, garantia, especificaciones) VALUES
+    ("LG Power",1500,12,'{"colores":["rojo","verde","azul"]}');
+
+SELECT * FROM equipos;
+
+-- Active: 1668121920190@@127.0.0.1@3306@experimentos
+
+SELECT * FROM equipos;
+
+
+SELECT nombre, JSON_EXTRACT(especificaciones,'$.capacidad') FROM equipos;
+
+SELECT nombre, JSON_EXTRACT(especificaciones,'$.ram') FROM equipos
+
+
+INSERT INTO equipos (nombre, precio, garantia, especificaciones) VALUES
+    ("LG Power",1500,12,'{"colores":["rojo","verde","azul"]}');
+
+SELECT nombre,
+    JSON_EXTRACT(especificaciones,'$.colores') AS 'colores'
+ FROM equipos 
+ WHERE idequipo = 2;
+
+ -- RECUPERAMOS EL PRIMER VALOR
+ SELECT nombre,
+    JSON_EXTRACT(especificaciones,'$.colores[0]') AS 'colores'
+ FROM equipos 
+ WHERE idequipo = 2;
+
+ -- CAMBIAR EL ULTIMO COLOR => rojo => blanco
+
+ UPDATE equipos SET
+ especificaciones = JSON_REPLACE(especificaciones,'$.colores[0]', "blanco")
+    WHERE idequipo = 2;
--- views/js/utilidades.js
+++ views/js/utilidades.js
@@ -1,14 +1,14 @@
 function mostrarAlerta(texto){
     Swal.fire({
-    icon: 'warning',
-    title: texto,
-    position: 'bottom-end',
-    background: '#FF8E8E',
-    //text: 'Proceso finalizado',
-    //footer: '<a class="btn btn-danger" href="https://www.youtube.com">A donde desea ir? YT</a>',
-    timer: 1100,
-    toast:true,
-    showConfirmButton:false
+        icon: 'warning',
+        title: texto,
+        position: 'bottom-end',
+        background: '#FF8E8E',
+        //text: 'Proceso finalizado',
+        //footer: '<a class="btn btn-danger" href="https://www.youtube.com">A donde desea ir? YT</a>',
+        timer: 1100,
+        toast:true,
+        showConfirmButton:false
     });
 }
 
